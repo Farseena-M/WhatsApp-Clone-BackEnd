98c1fd348eb377cfc2261c97d26cb09a410d0793
Farseena-M <farseenamoyan19@gmail.com>
2024-05-05 21:08:00 +0530

    updt19

--- src/apis/socket/controller/room.js
+++ src/apis/socket/controller/room.js
@@ -4,29 +4,29 @@ const { io } = require('../socket');
 
 const handleJoinRoom = (socket) => {
     const createRoom = () => {
-        const roomId = uuidV4()
-        socket.emit('room-created', { roomId })
-        console.log('User create the room');
-    }
+        const roomId = uuidV4();
+        socket.emit('room-created', { roomId });
+        console.log('User created the room');
+    };
 
     const joinRoom = ({ roomId }) => {
         console.log('User joined the room', roomId);
-        socket.join(roomId)
+        socket.join(roomId);
     };
 
-    socket.on('create-room', createRoom)
-    socket.on('join-room', joinRoom)
+    socket.on('create-room', createRoom);
+    socket.on('join-room', joinRoom);
 
     // Handle WebRTC signaling messages
-    socket.on('offer', ({ offer, roomId, caller, reciever }) => {
-        const callerDtls = getOneUser(caller)
-        const recieverDtls = getOneUser(reciever)
+    socket.on('offer', ({ offer, roomId, caller, receiver }) => {
+        const callerDetails = getOneUser(caller);
+        const receiverDetails = getOneUser(receiver);
         const data = {
             offer: offer,
             roomId: roomId,
-            caller: callerDtls,
-            reciever: recieverDtls
-        }
+            caller: callerDetails,
+            receiver: receiverDetails
+        };
         if (io) {
             io.to(roomId).emit('offer', data);
         } else {
@@ -34,14 +34,14 @@ const handleJoinRoom = (socket) => {
         }
     });
 
-    socket.on('answer', ({ roomId, answerer, reciever, answer }) => {
-        const answeredUser = getOneUser(answerer)
-        const recievedUser = getOneUser(reciever)
+    socket.on('answer', ({ roomId, answerer, receiver, answer }) => {
+        const answeredUser = getOneUser(answerer);
+        const receivedUser = getOneUser(receiver);
         const data = {
             answerer: answeredUser,
-            recievedUser: recievedUser,
+            receiver: receivedUser,
             answer: answer
-        }
+        };
         if (io) {
             io.to(roomId).emit('answer', data);
         } else {
@@ -49,14 +49,14 @@ const handleJoinRoom = (socket) => {
         }
     });
 
-    socket.on('ice-candidate', ({ roomId, sender, reciever, candidate }) => {
-        const sendedUser = getOneUser(sender)
-        const recievedUser = getOneUser(reciever)
+    socket.on('ice-candidate', ({ roomId, sender, receiver, candidate }) => {
+        const senderUser = getOneUser(sender);
+        const receiverUser = getOneUser(receiver);
         const data = {
-            sender: sendedUser,
-            reciever: recievedUser,
+            sender: senderUser,
+            receiver: receiverUser,
             candidate: candidate
-        }
+        };
         if (io) {
             io.to(roomId).emit('ice-candidate', data);
         } else {
@@ -64,8 +64,39 @@ const handleJoinRoom = (socket) => {
         }
     });
 
+    // Handle invitation, acceptance, and rejection
+    socket.on('send-invitation', ({ invitationSender, invitationReceiver }) => {
+        const sender = getOneUser(invitationSender);
+        const receiver = getOneUser(invitationReceiver);
+        const data = {
+            invitationSender: sender,
+            invitationReceiver: receiver
+        };
+        // Emit invitation event to the receiver
+        io.to(invitationReceiver).emit('invitation', data);
+    });
 
+    socket.on('accept-invitation', ({ answerer, receiver }) => {
+        const inviteAnswerer = getOneUser(answerer);
+        const answerReceiver = getOneUser(receiver);
+        const data = {
+            inviteAnswerer,
+            answerReceiver
+        };
+        // Notify both parties that the invitation was accepted
+        io.to(receiver).emit('invitation-accepted', data);
+    });
 
+    socket.on('reject-invitation', ({ rejecter, receiver }) => {
+        const inviteRejecter = getOneUser(rejecter);
+        const rejectReceiver = getOneUser(receiver);
+        const data = {
+            inviteRejecter,
+            rejectReceiver
+        };
+        // Notify the caller that the invitation was rejected
+        io.to(receiver).emit('invitation-rejected', data);
+    });
 };
 
 module.exports = handleJoinRoom;
